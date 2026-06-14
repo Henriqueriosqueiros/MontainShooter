6fe5b545fd20a89cfe8a3cdb4eacc5bb49843233
CianeRamos <ciane.uninter@tantofaz.com>
2026-06-14 09:59:49 -0300

    Primeira parte do projeto

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print('Setup Started')
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+print('Setup Started')
+
+print('Loop Started')
+while True:
+    # check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # Close Window
+            quit()  # end pygame
